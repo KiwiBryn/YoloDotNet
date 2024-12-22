02201be984e8a6644a3224f19408110205d5b1c4
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2024-12-22 19:20:41 +1300

    YoloV9 less video handler

--- YoloDotNet/Modules/V9/ObjectDetectionModuleV9.cs
+++ YoloDotNet/Modules/V9/ObjectDetectionModuleV9.cs
@@ -25,21 +25,27 @@
         public List<ObjectDetection> ProcessImage(SKImage image, double confidence, double pixelConfidence, double iou)
             => _objectDetectionModule.ProcessImage(image, confidence, pixelConfidence, iou);
 
+        /*
         public Dictionary<int, List<ObjectDetection>> ProcessVideo(VideoOptions options, double confidence, double pixelConfidence, double iou)
             => _yoloCore.RunVideo(options, confidence, pixelConfidence, iou, ProcessImage);
+        */
 
         private void SubscribeToVideoEvents()
         {
+            /*
             _yoloCore.VideoProgressEvent += (sender, e) => VideoProgressEvent?.Invoke(sender, e);
             _yoloCore.VideoCompleteEvent += (sender, e) => VideoCompleteEvent?.Invoke(sender, e);
             _yoloCore.VideoStatusEvent += (sender, e) => VideoStatusEvent?.Invoke(sender, e);
+            */
         }
 
         public void Dispose()
         {
+            /*
             _yoloCore.VideoProgressEvent -= (sender, e) => VideoProgressEvent?.Invoke(sender, e);
             _yoloCore.VideoCompleteEvent -= (sender, e) => VideoCompleteEvent?.Invoke(sender, e);
             _yoloCore.VideoStatusEvent -= (sender, e) => VideoStatusEvent?.Invoke(sender, e);
+            */
 
             _yoloCore?.Dispose();
 
